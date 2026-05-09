3260dd0c863eb97d87b26cbe08a0ece62151cffc
raajamit10 <rajamit6565@gmail.com>
2026-05-09 07:45:08 +0530

    update login styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,12 @@ import { supabase } from "../services/supabase";
 import "../styles/login.css";
 
 function Login() {
+
   const handleGoogleLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        // IMPORTANT: works on both localhost + Vercel
-        redirectTo: `${window.location.origin}/profile`,
+        redirectTo: "http://localhost:5173/profile",
       },
     });
 
@@ -19,14 +19,22 @@ function Login() {
 
   return (
     <div className="login-container">
+
       <div className="login-box">
+
         <h2>🚨 Safe Signal</h2>
+
         <p>Emergency SOS Protection System</p>
 
-        <button className="google-btn" onClick={handleGoogleLogin}>
+        <button
+          className="google-btn"
+          onClick={handleGoogleLogin}
+        >
           Sign in with Google
         </button>
+
       </div>
+
     </div>
   );
 }
